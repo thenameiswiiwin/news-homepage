ddc0475673d2a04e81b470e44a8a046fc6c5d154
Huy Nguyen <huyn.nguyen95@gmail.com>
2022-12-07 21:13:16 -0600

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{ts,tsx}'],
+  theme: {
+    colors: {
+      white: '#fffdfa',
+      blue: '#00001a',
+      gunmetal: '#5e607a',
+      vermillion: '#f15d51',
+      silver: '#c5c6ce',
+      yellow: '#e9aa52',
+    },
+    extend: {},
+  },
+  plugins: [],
+};
