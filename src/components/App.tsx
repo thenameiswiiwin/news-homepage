63e4c2f324f674e493dee4a9be35dec9dc5f2b3b
Huy Nguyen <huyn.nguyen95@gmail.com>
2022-12-07 22:01:18 -0600

    design system

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,7 @@
 const App = () => {
   return (
     <div>
-      <h1 className="text-red-500">Hello</h1>
+      <h1>Hello</h1>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,42 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./index.html', './src/**/*.{ts,tsx}'],
   theme: {
+    screens: {
+      lg: '1440px',
+    },
     colors: {
+      background: '#f2f2f2',
       white: '#fffdfa',
-      blue: '#00001a',
+      dark: '#00001a',
       gunmetal: '#5e607a',
       vermillion: '#f15d51',
       silver: '#c5c6ce',
       yellow: '#e9aa52',
     },
-    extend: {},
+    fontSize: {
+      xs: ['.875rem', '1.5rem'], // 14px
+      base: ['0.9375rem', '1.625rem'], // 15px
+      sm: ['1.125rem', '1.5rem'], // 18px
+      md: ['1.25rem', '1.5rem'], // 20px
+      lg: ['2rem', '2rem'], // 32px
+      xl: ['2.5rem', '2.5rem'], // 40px
+      '2xl': ['3.5rem', '3.5rem'], // 56px
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+      },
+      padding: {
+        13: '0.8125rem', // 13px
+        31: '1.9375rem', // 31px
+      },
+      letterSpacing: {
+        btn: '4.375px',
+      },
+    },
   },
   plugins: [],
 };
